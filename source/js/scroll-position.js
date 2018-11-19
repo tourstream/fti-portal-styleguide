98fa6d74037a6bc1f3eaf8402d7f6e4edc3a3f68
BirgitPohl <trash.birgitpohl@gmail.com>
2018-11-19 17:17:42 +0100

    ETS-1445 navigation styling

--- source/js/scroll-position.js
+++ source/js/scroll-position.js
@@ -1,14 +1,16 @@
 var scrollPosition;
 var bodyClasses = document.querySelector("body").classList;
 var htmlClasses = document.querySelector("html").classList;
+
+// Todo part of Ticket ETS-1382 second level navigation
 function saveScrollPosition() {
-  // bodyClasses.add("no-scroll");
-  // htmlClasses.add("no-scroll");
+  bodyClasses.add("no-scroll");
+  htmlClasses.add("no-scroll");
   scrollPosition = window.scrollY;
 }
 
-function returnToScrollposition () {
-  // htmlClasses.remove("no-scroll");
-  // htmlClasses.remove("no-scroll");
+function returnToScrollPosition () {
+  htmlClasses.remove("no-scroll");
+  bodyClasses.remove("no-scroll");
   window.scrollTo(0, scrollPosition);
 }
