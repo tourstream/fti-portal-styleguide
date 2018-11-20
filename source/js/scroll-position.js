9f5ea13aa5998bce200df641c32b526dd8b89dcc
BirgitPohl <trash.birgitpohl@gmail.com>
2018-11-20 16:41:42 +0100

    Fixing lints - EsLint does not work properly with pattern lab

--- source/js/scroll-position.js
+++ source/js/scroll-position.js
@@ -3,13 +3,13 @@ var bodyClasses = document.querySelector("body").classList;
 var htmlClasses = document.querySelector("html").classList;
 
 // Todo part of Ticket ETS-1382 second level navigation
-function saveScrollPosition() {
+function saveScrollPosition() { // eslint-disable-line no-unused-vars
   bodyClasses.add("no-scroll");
   htmlClasses.add("no-scroll");
   scrollPosition = window.scrollY;
 }
 
-function returnToScrollPosition () {
+function returnToScrollPosition () { // eslint-disable-line no-unused-vars
   htmlClasses.remove("no-scroll");
   bodyClasses.remove("no-scroll");
   window.scrollTo(0, scrollPosition);
