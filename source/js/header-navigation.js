dd900c8ea5709707ae098198fac62f42f1ba6224
BirgitPohl <trash.birgitpohl@gmail.com>
2018-12-06 16:24:06 +0100

    ETS-1387 mobile second level navigation opens and closes

--- source/js/header-navigation.js
+++ source/js/header-navigation.js
@@ -4,6 +4,7 @@ var openMenuClasses;
 
 openMenuClasses = document.querySelector(".menu-open").classList;
 
+
 const body = document.querySelector("body");
 
 navigationElements.push(
@@ -28,8 +29,23 @@ var closeNavigation = function () {
   body.classList.remove("unscrollable");
 };
 
+var toggleSubMenu = function(element) {
+  var subMenus = document.querySelectorAll(".header-sub-menu-container");
+  var menuLinks = document.querySelectorAll(".header-menu-item-link");
+  subMenus.forEach(function(subMenu) {
+    subMenu.classList.remove("display-block");
+  });
+  menuLinks.forEach(function(subMenu) {
+    subMenu.classList.remove("active");
+  });
+
+  element.querySelector("ul").classList.add("display-block");
+  element.querySelector("a").classList.add("active");
+};
+
 
 module.exports = {
   openNavigation: openNavigation,
-  closeNavigation: closeNavigation
+  closeNavigation: closeNavigation,
+  toggleSubMenu, toggleSubMenu
 };
