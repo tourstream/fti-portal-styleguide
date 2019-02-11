98aec2c9645a1392f210b886f1779ad05cea655b
Filipe Espósito <filipe.tessariesposito@tourstream.eu>
2019-02-11 11:59:10 +0100

    ETS-1660 Fixes JS bug related to the Skyscraper ad

--- source/js/google-ads.js
+++ source/js/google-ads.js
@@ -2,7 +2,10 @@ var adjustSkyscraperPositioning = function() {
   var mainElement = document.getElementsByTagName("main")[0];
   if (mainElement) {
     var newHeight = mainElement.offsetTop;
-    document.getElementsByClassName("ad-skyscraper-wrapper")[0].style.top = newHeight + "px";
+    var skyscraper = document.getElementsByClassName("ad-skyscraper-wrapper")[0];
+    if (skyscraper) {
+      skyscraper.style.top = newHeight + "px";
+    }
   }
 };
 
