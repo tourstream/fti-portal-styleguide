ee23ea8873e819abcb888069bec9b3660f3805f2
Filipe Espósito <filipe.tessariesposito@tourstream.eu>
2019-02-25 09:29:30 +0100

    NO-TICKET fixes JS code for Google Ads

--- source/js/google-ads.js
+++ source/js/google-ads.js
@@ -1,7 +1,7 @@
 /* global errorHandling:false */
 var adjustSkyscraperPositioning = function() {
   var mainElement = document.getElementsByTagName("main")[0];
-  var skyscraper = document.querySelector("ad-skyscraper-wrapper");
+  var skyscraper = document.getElementsByClassName('ad-skyscraper-wrapper')[0];
   var guardMainElement = errorHandling.checkElement(mainElement, function(){return true;});
   var guardSkyscraper = errorHandling.checkElement(skyscraper, function(){return true;});
 
